c83fa47565c6836094ded36770e495690995d055
Richard Gusmao <richardgusmaoalberto@gmail.com>
2023-12-18 15:39:24 +0100

    Creating the project struc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
 
-import {CTA, Brand, Navbar, Feature, Article} from "./components";
+import {CTA, Brand, Navbar} from './components';
 
+import './App.css';
+import './index.css';
 const App = () => {
     return (
-        <div>
-            <h1>GTP-3</h1>
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar/>
+                <Header/>
+            </div>
+            <Brand/>
+            <WhatGPT3/>
+            <Features/>
+            <Possibility/>
+            <CTA/>
+            <Blog/>
+            <Footer/>
         </div>
     );
 }
---
+++ src/containers/whatGTP3/WhatGTP3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WhatGTP3 = () => {
+  return (
+    <div>WhatGTP3</div>
+  )
+}
+
+export default WhatGTP3
